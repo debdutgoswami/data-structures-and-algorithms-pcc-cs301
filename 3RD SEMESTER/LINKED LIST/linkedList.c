d2b4471b1a8a2d32795b9faf8eba82bd8afc1e5e
Debdut Goswami <debdutgoswami@gmail.com>
2020-12-22 11:54:54 +0530

    some more functions

--- 3RD SEMESTER/LINKED LIST/linkedList.c
+++ 3RD SEMESTER/LINKED LIST/linkedList.c
@@ -1,138 +1,226 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
 	int data;
 	struct node *next;
-}*start=NULL;
-struct node* create(int d){
+} *start = NULL;
+struct node *create(int d)
+{
 	struct node *n;
-	n=(struct node*)malloc(sizeof(struct node));
-	n->data=d;
-	n->next=NULL;
+	n = (struct node *)malloc(sizeof(struct node));
+	n->data = d;
+	n->next = NULL;
 	return n;
 }
-void insertFront(struct node *n){
-	if(start==NULL)
-		start=n;
-	else{
-		n->next=start;
-		start=n;
-	}		
-}
-void insertRear(struct node *n){
-	struct node *ptr=start;
-	if(start==NULL)
-		start=n;
-	else{
-		while(ptr->next!=NULL)
-			ptr=ptr->next;
-		ptr->next=n;
+void insertFront(struct node *n)
+{
+	if (start == NULL)
+		start = n;
+	else
+	{
+		n->next = start;
+		start = n;
 	}
 }
-void insertPos(struct node *n, int pos){
+void insertRear(struct node *n)
+{
+	struct node *ptr = start;
+	if (start == NULL)
+		start = n;
+	else
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = n;
+	}
+}
+void insertPos(struct node *n, int pos)
+{
 	struct node *ptr;
-	if(start==NULL)
-		start=n;
-	else{
-		struct node *ptr,*prev;
-		int i=1;
-		for(ptr=start;ptr!=NULL&&i<pos-1;prev=ptr,ptr=ptr->next,i++);
-		if(ptr==start){
-			n->next=start;
-			start=n;
-		}else{
-			prev->next=n;
-			n->next=ptr->next;	
+	if (start == NULL)
+		start = n;
+	else
+	{
+		struct node *ptr, *prev;
+		int i = 1;
+		for (ptr = start; ptr != NULL && i < pos - 1; prev = ptr, ptr = ptr->next, i++)
+			;
+		if (ptr == start)
+		{
+			n->next = start;
+			start = n;
+		}
+		else
+		{
+			prev->next = n;
+			n->next = ptr->next;
 		}
 	}
 }
-void show(){
-	if(start==NULL)
+void show()
+{
+	if (start == NULL)
 		printf("Underflow\n");
-	else{
+	else
+	{
 		struct node *ptr;
 		printf("The list is ");
-		for(ptr=start;ptr!=NULL;ptr=ptr->next)
-			printf("%d ",ptr->data);
+		for (ptr = start; ptr != NULL; ptr = ptr->next)
+			printf("%d ", ptr->data);
 		printf("\n");
 	}
 }
-void del(int d){
-	if(start==NULL)
+void del(int d)
+{
+	if (start == NULL)
 		printf("Underflow\n");
-	else{
+	else
+	{
 		struct node *ptr, *prev;
-		for(ptr=start;ptr!=NULL;prev=ptr,ptr=ptr->next)
-			if(ptr->data==d)
+		for (ptr = start; ptr != NULL; prev = ptr, ptr = ptr->next)
+			if (ptr->data == d)
 				break;
-		if(ptr==NULL)
+		if (ptr == NULL)
 			printf("Not Found\n");
-		else{
-			printf("%d is deleted\n",d);
-			if(ptr==start)
-				start=ptr->next;
+		else
+		{
+			printf("%d is deleted\n", d);
+			if (ptr == start)
+				start = ptr->next;
 			else
-				prev->next=ptr->next;
+				prev->next = ptr->next;
 			free(ptr);
 		}
 	}
 }
-void rev(){
-	if(start==NULL)
+void rev()
+{
+	if (start == NULL)
 		printf("Underflow\n");
-	else{
-		struct node *ptr=start, *prev=NULL, *tmp=NULL;
-		while(ptr!=NULL){
-			tmp=ptr->next;
-			ptr->next=prev;
-			
-			prev=ptr;
-			ptr=tmp;
+	else
+	{
+		struct node *ptr = start, *prev = NULL, *tmp = NULL;
+		while (ptr != NULL)
+		{
+			tmp = ptr->next;
+			ptr->next = prev;
+
+			prev = ptr;
+			ptr = tmp;
 		}
-		start=prev;
+		start = prev;
 		printf("The list is reversed\n");
 	}
 }
-int main(void){
-	int n,d,pos;
-	do{
-		printf("Menu:\n1. Insert at Front\n2. Insert at Rear\n3. Insert anywhere\n4. Show\n5. Delete\n6. Reverse\n7. Exit\nEnter: ");
-			scanf("%d",&n);
-		switch(n){
-			case 1:
-				printf("enter the data: ");
-					scanf("%d",&d);
-				insertFront(create(d));
-				break;
-			case 2:
-				printf("enter the data: ");
-					scanf("%d",&d);
-				insertRear(create(d));
-				break;
-			case 3:
-				printf("enter the data: ");
-					scanf("%d",&d);
-				printf("enter the position: ");
-					scanf("%d",&pos);
-				insertPos(create(d),pos);
-				break;
-			case 4:
-				show();
-				break;
-			case 5:
-				printf("enter the data: ");
-					scanf("%d",&d);
-				del(d);
-				break;
-			case 6:
-				rev();
-				break;
-			case 7:
-				printf("Thank You\n");
-				break;
-			default:
-				printf("Wrong Choice!! Try again noob\n");
+int count()
+{
+	struct node *ptr = start;
+	int c = 0;
+	while (ptr != NULL)
+	{
+		c++;
+		ptr = ptr->next;
+	}
+	return c;
+}
+void atEven()
+{
+	struct node *ptr = start;
+	int i = 0;
+	printf("Considering 0-th index as even\n");
+	while (ptr != NULL)
+	{
+		if (i % 2 == 0)
+			printf("%d ", ptr->data);
+		i++;
+		ptr = ptr->next;
+	}
+	printf("\n");
+}
+void atOdd()
+{
+	struct node *ptr = start;
+	int i = 0;
+	printf("Considering 0-th index as even\n");
+	while (ptr != NULL)
+	{
+		if (i % 2 != 0)
+			printf("%d ", ptr->data);
+		i++;
+		ptr = ptr->next;
+	}
+	printf("\n");
+}
+void swapFirstLast()
+{
+	if (start == NULL || start->next == NULL)
+		return;
+	struct node *ptr = start;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	// swaping by value
+	int tmp = start->data;
+	start->data = ptr->data;
+	ptr->data = tmp;
+}
+int main(void)
+{
+	int n, d, pos;
+	do
+	{
+		printf("Menu:\n1. Insert at Front\n2. Insert at Rear\n3. Insert anywhere\n4. Show\n5. Delete\n6. Reverse\n7. Count\n8. Even\n9. Odd\n10. Swap\n11. Exit\nEnter: ");
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:
+			printf("enter the data: ");
+			scanf("%d", &d);
+			insertFront(create(d));
+			break;
+		case 2:
+			printf("enter the data: ");
+			scanf("%d", &d);
+			insertRear(create(d));
+			break;
+		case 3:
+			printf("enter the data: ");
+			scanf("%d", &d);
+			printf("enter the position: ");
+			scanf("%d", &pos);
+			insertPos(create(d), pos);
+			break;
+		case 4:
+			show();
+			break;
+		case 5:
+			printf("enter the data: ");
+			scanf("%d", &d);
+			del(d);
+			break;
+		case 6:
+			rev();
+			break;
+		case 7:
+			printf("Number of nodes: %d\n", count());
+			break;
+		case 8:
+			printf("Even:\n");
+			atEven();
+			break;
+		case 9:
+			printf("Odd:\n");
+			atOdd();
+			break;
+		case 10:
+			swapFirstLast();
+			break;
+		case 11:
+			printf("Thank You\n");
+			break;
+		default:
+			printf("Wrong Choice!! Try again noob\n");
 		}
-	}while(n!=7);
+	} while (n != 7);
 	return 1;
 }
